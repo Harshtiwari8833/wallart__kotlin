db31a0f8d23c9d73fb39d4954a29cc1cf758ba24
harshtiwari <harshtiwari8833@gmail.com>
2023-08-08 23:20:48 +0530

    change package

--- app/src/main/java/com/maverickbits/wallart/MainActivity.kt
+++ app/src/main/java/com/maverickbits/wallart/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.maverickbits.wallart
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(applicationContext,"this is toast message",Toast.LENGTH_SHORT).show()
     }
 }
