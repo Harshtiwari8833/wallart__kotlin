218f706266a4f59ef46cf57880f5891b422876c5
nikhilpanchal <nikhil.parasf.9911@gmail.com>
2023-08-09 21:44:29 +0530

    New

--- app/src/main/java/com/maverickbits/wallart/category_fragment.kt
+++ app/src/main/java/com/maverickbits/wallart/category_fragment.kt
@@ -5,19 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.maverickbits.wallart.databinding.FragmentCategoryBinding
 
 class category_fragment : Fragment() {
 
+    private lateinit var binding : FragmentCategoryBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category, container, false)
+
+        binding = FragmentCategoryBinding.inflate(inflater,container,false)
+
+        binding.cardNature.setOnClickListener {
+
+
+        }
 
 
 
+        return binding.root
     }
 
 }
