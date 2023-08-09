3c84fcd99b016862e85413b5ad95f4de0c890fab
nikhilpanchal <nikhil.parasf.9911@gmail.com>
2023-08-09 19:53:35 +0530

    New

---
+++ app/src/main/java/com/maverickbits/wallart/profile_fragment.kt
@@ -0,0 +1,32 @@
+package com.maverickbits.wallart
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.maverickbits.wallart.databinding.ActivityMainBinding
+import com.maverickbits.wallart.databinding.FragmentProfileBinding
+
+
+class profile_fragment : Fragment() {
+
+    private lateinit var binding: FragmentProfileBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        binding.cardAbout.setOnClickListener{
+
+            val iAbout = Intent (activity,About_Activity::class.java)
+            startActivity(iAbout)
+        }
+        return binding.root
+
+    }
+}
