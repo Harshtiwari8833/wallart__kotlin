cb3eb4187957842e4f2f44d0e2d8040140f751ee
Ankur Tiwari <ankurtiwar87@gmail.com>
2023-08-10 15:40:27 +0530

    Adapter set

---
+++ app/src/main/java/com/maverickbits/wallart/Adapter/WallAdapter.kt
@@ -0,0 +1,34 @@
+package com.maverickbits.wallart.Adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.maverickbits.wallart.Models.WallModel
+import com.maverickbits.wallart.R
+import com.maverickbits.wallart.databinding.WallLayoutBinding
+
+class WallAdapter(private val context: Context,private val list :ArrayList<WallModel>)
+    :RecyclerView.Adapter<WallAdapter.ViewHolder>() {
+
+        inner class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView)
+        {
+            val binding = WallLayoutBinding.bind(itemView)
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.wall_layout,parent,false))
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentItem = list[position]
+        Glide.with(context).load(currentItem).into(holder.binding.wallImg)
+
+    }
+}
---
+++ app/src/main/java/com/maverickbits/wallart/Models/WallModel.kt
@@ -0,0 +1,8 @@
+package com.maverickbits.wallart.Models
+
+data class WallModel (
+
+    val imgUrl:String?="",
+    val cat:String?=""
+
+)
