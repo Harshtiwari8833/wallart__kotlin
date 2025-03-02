592b165c38d072b586b697e6da40a8199287861c
Ankur Tiwari <ankurtiwar87@gmail.com>
2025-03-03 01:04:17 +0530

    api url changed

--- app/src/main/java/com/maverickbits/wallart/Api/ApiUtilities.kt
+++ app/src/main/java/com/maverickbits/wallart/Api/ApiUtilities.kt
@@ -5,7 +5,10 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiUtilities {
 
-    private const val BASE_URL="https://busy-tan-piranha-robe.cyclic.app/"
+//    private const val BASE_URL="https://busy-tan-piranha-robe.cyclic.app/"
+
+
+    private const val BASE_URL="https://wallart-5ccj.onrender.com/"
     fun getInstance(): Retrofit {
 
         return Retrofit.Builder()
@@ -13,4 +16,5 @@ object ApiUtilities {
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
+
 }
--- app/src/main/java/com/maverickbits/wallart/utils/Constants.kt
+++ app/src/main/java/com/maverickbits/wallart/utils/Constants.kt
@@ -1,5 +1,6 @@
 package com.maverickbits.wallart.utils
 
 object Constants  {
-    const val BASE_URL = "https://busy-tan-piranha-robe.cyclic.app/"
+//    const val BASE_URL = "https://busy-tan-piranha-robe.cyclic.app/"
+    const val BASE_URL = "https://wallart-5ccj.onrender.com/"
 }
